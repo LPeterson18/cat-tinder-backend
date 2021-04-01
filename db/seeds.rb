6454e2b832f8ca0b858c7fc454d7eb6dfa47876a
kevinnarbas <kevinnarbas@gmail.com>
2021-04-01 11:30:33 -0700

    differentiates seed content in seeds.rb to frontend mock data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,23 @@
 
 cats = [
   {
-    name: 'Mittens',
+    name: 'Duck',
     age: 5,
-    enjoys: 'sunshine and warm spots',
+    enjoys: 'window sill sitting',
     profile_pic:
       'https://images.unsplash.com/photo-1574158622682-e40e69881006?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
   },
   {
-    name: 'Raisins',
+    name: 'Craisins',
     age: 4,
-    enjoys: 'being queen of the dogs',
+    enjoys: 'being queen of the fish',
     profile_pic:
       'https://images.unsplash.com/photo-1500259571355-332da5cb07aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
   },
   {
-    name: 'Toast',
+    name: 'Burnt Toast',
     age: 1,
-    enjoys: 'getting all the attention',
+    enjoys: 'getting all the attention from strangers only',
     profile_pic:
       'https://images.unsplash.com/photo-1513245543132-31f507417b26?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80',
   },
